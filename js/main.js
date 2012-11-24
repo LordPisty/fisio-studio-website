f3057c2bfb397ae8df396c39154c9ae6d622cf87
paolo <paolo@ubuntu>
2012-11-24 17:22:20 +0100

    Inserite immagini e fix js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$(function() {
+	 $('.buttonItem').hover(function(event){
+		 var button = event.target;
+		 var nomeFile = button.id.substr(6).toLowerCase();
+	     $(button).attr('src',"img/button/"+nomeFile+"Hover.png");
+	     return false;
+	 },
+	 function(event){
+		 var button = event.target;
+		 var nomeFile = button.id.substr(6).toLowerCase();
+		 $(button).attr('src',"img/button/"+nomeFile+".png");
+	     return false;
+	 });
+	});
 
+$(function() {
+	 $('.buttonItem').click(function(event){
+		 var button = event.target;
+		 var nomeFile = button.id.substr(6).toLowerCase();
+	     $(button).attr('src',"img/button/"+nomeFile+"OnClick.png");
+	     return false;
+	 });
+	});
